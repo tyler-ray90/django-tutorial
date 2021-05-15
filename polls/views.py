d804e0d7b2410bc8a9e8cb9eda8c8208c0172a2f
tyler-ray90 <tyler_ray98@yahoo.com>
2021-05-15 18:34:32 -0400

    made urls within urls.py

---
+++ polls/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request): 
+    return HttpResponse("Hello World. You're at the polls index")
+
+def show(request):
+    return HttpResponse("Hello, this is the show view")
+
